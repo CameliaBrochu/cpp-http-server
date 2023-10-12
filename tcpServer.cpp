31fef7b32b70ae4fa714a805d6390cf86a7d4c3e
Camélia Brochu <cameliabrochu@gmail.com>
2023-10-12 19:33:53 -0400

    listen and connections

--- tcpServer.cpp
+++ tcpServer.cpp
@@ -1,5 +1,6 @@
 #include <winsock.h>
 #include <iostream>
+#include <sstream>
 #include "tcpServer.h"
 
 namespace cpp_http_server{
@@ -23,7 +24,7 @@ namespace cpp_http_server{
         this->socketAddr.sin_port = 8080;
         this->socketAddr.sin_addr.s_addr = inet_addr("0.0.0.0");
 
-        err = bind(this->tcpSocket, (sockaddr *)&(this->socketAddr), sizeof(this->socketAddr));
+        err = bind(this->tcpSocket, (sockaddr*)&(this->socketAddr), sizeof(this->socketAddr));
 
         if(err < 0){
             exitWithError("Cannot connect socket to address");
@@ -43,6 +44,31 @@ namespace cpp_http_server{
         std::cout << WSAGetLastError() << std::endl;
         log("ERROR: " + error);
         exit(1);
+    }
+
+    void TcpServer::startListen() {
+        int err = listen(this->tcpSocket,20);
+        if(err < 0){
+            exitWithError("Socket listen fail");
+        }
+
+        std::ostringstream ss;
+        ss << "\n Listening on ADDRESS: " << inet_ntoa(this->socketAddr.sin_addr)
+           << " PORT: " << ntohs(this->socketAddr.sin_port)
+           << "\n\n";
+
+        log(ss.str());
+    }
+
+    void TcpServer::acceptConnection() {
+        SOCKET newSocket = accept(this->tcpSocket,(sockaddr*)&(this->socketAddr), (int*)sizeof(this->socketAddr));
+
+        if(newSocket == INVALID_SOCKET){
+            std::ostringstream ss;
+            ss << "Server failed to accept incoming connection from " << inet_ntoa(this->socketAddr.sin_addr)
+            << " PORT: " << ntohs(this->socketAddr.sin_port);
+            exitWithError(ss.str());
+        }
     }
 
 }
--- tcpServer.h
+++ tcpServer.h
@@ -13,6 +13,8 @@ namespace cpp_http_server{
         SOCKET tcpSocket{};
         sockaddr_in socketAddr{};
 
+        void startListen();
+        void acceptConnection();
         static void log(const std::string &message);
         static void exitWithError(const std::string &error);
     };
